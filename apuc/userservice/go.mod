e1438beafaceb5db85a1cfcd47dd23dc85320f13
mukhammadkodir <qodircodermmi@gmail.com>
2022-08-17 10:05:20 +0500

    kafka added

--- apuc/userservice/go.mod
+++ apuc/userservice/go.mod
@@ -6,22 +6,25 @@ require (
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/lib/pq v1.10.5
-	github.com/spf13/cast v1.4.1
-	github.com/stretchr/testify v1.7.1
-	go.uber.org/zap v1.21.0
-	google.golang.org/grpc v1.45.0
+	github.com/lib/pq v1.10.6
+	github.com/segmentio/kafka-go v0.4.34
+	github.com/spf13/cast v1.5.0
+	github.com/stretchr/testify v1.8.0
+	go.uber.org/zap v1.22.0
+	google.golang.org/grpc v1.48.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/compress v1.15.7 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/net v0.0.0-20220706163947-c90051bbdb60 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- apuc/userservice/service/user.go
+++ apuc/userservice/service/user.go
@@ -2,31 +2,47 @@ package service
 
 import (
 	"context"
+	"fmt"
 
+	p "github/Services/apuc/userservice/genproto/post_service"
 	pb "github/Services/apuc/userservice/genproto/user_service"
 	l "github/Services/apuc/userservice/pkg/logger"
+	"github/Services/apuc/userservice/pkg/messagebroker"
 	grpcClient "github/Services/apuc/userservice/service/grpcclient"
-	p "github/Services/apuc/userservice/genproto/post_service"
 
 	"github/Services/apuc/userservice/storage"
 
 	"github.com/jmoiron/sqlx"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 //UserService ...
 type UserService struct {
-	storage storage.IStorage
-	logger  l.Logger
-	client  grpcClient.GrpcClientI
+	storage   storage.IStorage
+	logger    l.Logger
+	client    grpcClient.GrpcClientI
+	publisher map[string]messagebroker.Publisher
 }
 
 //NewUserService ...
-func NewUserService(db *sqlx.DB, log l.Logger, client grpcClient.GrpcClientI) *UserService {
+func NewUserService(db *sqlx.DB, log l.Logger, client grpcClient.GrpcClientI, publisher map[string]messagebroker.Publisher) *UserService {
 	return &UserService{
-		storage: storage.NewStoragePg(db),
-		logger:  log,
-		client:  client,
+		storage:   storage.NewStoragePg(db),
+		logger:    log,
+		client:    client,
+		publisher: publisher,
 	}
+}
+
+func (s *UserService) publisherUserMessage(user []byte) error {
+
+	err := s.publisher["user"].Publish([]byte("user"), user, string(user))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (s *UserService) Create(ctx context.Context, req *pb.User) (*pb.User, error) {
@@ -34,6 +50,22 @@ func (s *UserService) Create(ctx context.Context, req *pb.User) (*pb.User, error
 	if err != nil {
 		s.logger.Error("Failed create user", l.Error(err))
 		return nil, nil
+	}
+
+	p, _ := user.Marshal()
+	var usera pb.User
+	err = usera.Unmarshal(p)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	fmt.Println(user)
+
+	err = s.publisherUserMessage(p)
+	if err != nil {
+		s.logger.Error("failed while publishing user info", l.Error(err))
+		return nil, status.Error(codes.Internal, "failed while publishing user info")
+
 	}
 
 	return user, nil
